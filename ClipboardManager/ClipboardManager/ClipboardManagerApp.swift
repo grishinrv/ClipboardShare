f47a10796da991f452870cf625be85e52892c87b
RomanGrishin <roman1988grishin@gmail.com>
2022-08-02 18:32:07 +0300

    Swift UI project template

---
+++ ClipboardManager/ClipboardManager/ClipboardManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClipboardManagerApp.swift
+//  ClipboardManager
+//
+//  Created by Roman Grishin on 2.8.2022.
+//
+
+import SwiftUI
+
+@main
+struct ClipboardManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
